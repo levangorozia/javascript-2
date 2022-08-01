ffdede93ce7a89d8129d58d84e6475379fdb0f9a
levangorozia <106963464+levangorozia@users.noreply.github.com>
2022-08-01 16:07:46 +0400

    add davalebebi

---
+++ script.js
@@ -0,0 +1,86 @@
+
+// davaleba #1 - შექმენით ფუნქცია, რომელსაც გადავცემთ n რაოდენობის მნიშვნელობას და დააბრუნოს მხოლოდ დადებითი რიცხვების ჯამს:
+
+// function cifrebi (...numbers) {
+    
+//     let numbers_cifrebi = 0;
+//     for (let item of numbers){
+//         if (item > 0){
+//             numbers_cifrebi = numbers_cifrebi + item;
+//         }
+//     }
+
+// return numbers_cifrebi;
+
+// }
+// let result = cifrebi (2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+// console.log(result);
+
+
+
+// davaleba #2
+// function ricxvebi (...numbers){
+
+//     let numbers_ricxvebi = 0;
+
+// for (let item of numbers){
+
+//     numbers_ricxvebi += item
+// }
+//     console.log( numbers_ricxvebi);
+// }
+
+// ricxvebi (10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+
+
+// davaleba #3 - 
+
+// function giorgi_saakadze () {
+//    let user = {
+//       firstname: 'giorgi',
+//       lastname: 'saakadze',
+//       age: 32,
+//       isloggedin: true
+//   }
+     
+//      if (user.isloggedin == true) {
+
+//         return 'giorgi saakadze';
+
+//       } else if (user.isloggedin == false) 
+//         return'false'
+ 
+// } 
+
+// let result = giorgi_saakadze()
+// console.log(result);
+
+
+
+
+// davaleba #4 - შექმენით ფუნქცია რომელსაც გადაეცემა ერთი პარამეტრი. ეს პარამეტრი უნდა იყოს რიცხვების მასივი და 
+// ფუნქციამ უნდა დააბრუნოს ამ რიცხვებიდან მაქსიმალური:
+
+// function sum (...numbers) {
+
+//     let numbersum = 0;
+//     for (let item of numbers){
+//         numbersum = numbersum + item;
+//     }
+
+// return numbersum;
+
+// }
+// let result = sum (5, 10, 14, 100,);
+// console.log(result);
+
+
+
+// davaleba #5 - გამოიტანეთ ყველა ის რიცხვი რომელიც მეტია 0 ზე და ნაკლებია 10ზე;
+
+// let array = [1,2,4,10,34,5,7,87]
+
+// davaleba #6
